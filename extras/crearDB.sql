9621a739e9fbd6a9b1369df05fb81d7368a6bd89
David Ochoa <ochoasabogaldavid@gmail.com>
2025-04-28 00:14:43 -0500

    Update crearDB.sql

--- extras/crearDB.sql
+++ extras/crearDB.sql
@@ -33,7 +33,7 @@ CREATE TABLE boletoOfacturaViajes (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   idViaje INTEGER,
   nombreUsuario VARCHAR(40),
-  idUsuario INTEGER,
+  idUsuario BIGINT,
   equipaje BOOLEAN,
   cantidadEquipaje INTEGER,
   pesoEquipaje INTEGER,
@@ -44,9 +44,9 @@ CREATE TABLE facturaEnvios (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   idViaje INTEGER,
   nombreRemitente VARCHAR(40),
-  idRemitente INTEGER,
+  idRemitente BIGINT,
   nombreDestinatario VARCHAR(40),
-  idDestinatario INTEGER,
+  idDestinatario BIGINT,
   pesoPaquete INTEGER,
   FOREIGN KEY (idViaje) REFERENCES viajes(idViaje)
 );
@@ -55,7 +55,7 @@ CREATE TABLE pagosBoletos (
   idPago INTEGER PRIMARY KEY AUTO_INCREMENT,
   idBoleto INTEGER,
   metodoPago VARCHAR(40),
-  numeroTarjeta VARCHAR(20),
+  numeroTarjeta VARCHAR(60),
   precio INTEGER,
   FOREIGN KEY (idBoleto) REFERENCES boletoOfacturaViajes(id)
 );
@@ -64,7 +64,7 @@ CREATE TABLE pagosEnvios (
   idPago INTEGER PRIMARY KEY AUTO_INCREMENT,
   idFacturaEnvio INTEGER,
   metodoPago VARCHAR(40),
-  numeroTarjeta VARCHAR(20),
+  numeroTarjeta VARCHAR(60),
   precio INTEGER,
   FOREIGN KEY (idFacturaEnvio) REFERENCES facturaEnvios(id)
 );
