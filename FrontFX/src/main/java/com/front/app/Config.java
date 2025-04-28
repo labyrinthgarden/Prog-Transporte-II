a5969123cc1a6cbecdb00dcc228b58f963e92eae
David Ochoa <ochoasabogaldavid@gmail.com>
2025-04-28 00:39:05 -0500

    Delete FrontFX directory

--- FrontFX/src/main/java/com/front/app/Config.java
+++
@@ -1,6 +0,0 @@
-package com.front.app;
-
-public class Config {
-    public static final String URL_BACKEND = "https://prog-transporte-ii-production.up.railway.app";
-    //public static final String URL_BACKEND = "http://localhost:8081";
-}
